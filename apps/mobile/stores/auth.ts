75012b7458ef23ac6569f7a0b31fa0cd8e8ce6dd
jerrycearley@gmail.com <jerrycearley@gmail.com>
2026-02-26 20:24:54 -0800

    mobile: fix unused get param in auth store

--- apps/mobile/stores/auth.ts
+++ apps/mobile/stores/auth.ts
@@ -22,7 +22,7 @@ interface AuthState {
   loadStoredAuth: () => Promise<void>;
 }
 
-export const useAuthStore = create<AuthState>((set, get) => ({
+export const useAuthStore = create<AuthState>((set, _get) => ({
   user: null,
   accessToken: null,
   isLoading: true,
